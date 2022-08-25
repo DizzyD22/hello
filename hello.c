b3de10d8c6db303ae1449c2b644a0e67fe4e2be7
DizzyD22 <ddumont2@huskers.unl.edu>
2022-08-25 20:33:04 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Dizzy Dumont
+ *
+ * Date: 2022/08/29
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+
+  printf("Dizzy D\n");
+
+  return 0;
+}
